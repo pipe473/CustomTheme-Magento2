d20bc55b9a205f7b94d605d7a7d6a1f0188f96ce
pipe473 <bedoyafelipe743@gmail.com>
2021-02-14 12:08:24 +0100

    creando ejemplo de custom theme y definiendo links del block de header.links

---
+++ app/etc/env.php
@@ -0,0 +1,67 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'control'
+    ],
+    'crypt' => [
+        'key' => '33af89d9a9c7eb35a274a0ee034d26e5'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => '127.0.0.1:8889',
+                'dbname' => 'magento',
+                'username' => 'root',
+                'password' => 'root',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'f49_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'f49_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => NULL
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Sat, 13 Feb 2021 15:52:49 +0000'
+    ]
+];
